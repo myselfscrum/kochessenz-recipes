66ca91acfe9234f4ddfd6efa1a9ab759c5d2aaec
Selfscrum <martin@selfscrum.com>
2023-01-22 00:30:27 +0100

    submission

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -10,6 +10,11 @@ exports.handler = (event, context, callback) => {
   const payload = JSON.parse(event.body).payload
   console.log(payload.data)
   const { firstname, lastname, email, message, referrer } = payload.data
+  console.log (firstname)
+  console.log (lastname)
+  console.log (email)
+  console.log (message)
+  console.log (referrer)
   return { 
     statusCode: 200, 
     body: "Success!" 
