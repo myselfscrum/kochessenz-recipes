9f37385b11ac3ad9b5c6879dd0c9f130a6d12bf0
Selfscrum <martin@selfscrum.com>
2023-01-22 00:36:44 +0100

    submission

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -9,12 +9,14 @@ dayjs.extend(utc)
 exports.handler = (event, context, callback) => {
   const payload = JSON.parse(event.body).payload
   console.log(payload.data)
-  const { firstname, lastname, email, message, referrer } = payload.data
+  const { firstname, lastname, email, message, referrer, title, url } = payload.data
   console.log (firstname)
   console.log (lastname)
   console.log (email)
   console.log (message)
   console.log (referrer)
+  console.log (title)
+  console.log (url)
   return { 
     statusCode: 200, 
     body: "Success!" 
