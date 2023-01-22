2762c11f6f53c8e1bcd12168efe4ff8d8716db13
Selfscrum <martin@selfscrum.com>
2023-01-22 15:28:11 +0100

    utils

--- functions/utils/githubconnect.js
+++ functions/utils/githubconnect.js
@@ -4,12 +4,14 @@ const MyOctokit = Octokit.plugin(restEndpointMethods);
 
 const octokit = new MyOctokit({ auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN });
 
-const { data: rateLimitInfo } = octokit.request('GET /rate_limit', {})
-const remainingCalls = rateLimitInfo.resources.core.remaining;
-console.log(`GitHub API requests remaining: ${remainingCalls}`);
+const rateLimitInfo = async() => {
+    await octokit.request('GET /rate_limit', {})
+    const remainingCalls = rateLimitInfo.resources.core.remaining;
+    console.log(`GitHub API requests remaining: ${remainingCalls}`);
 
-if (remainingCalls === 0) {
+    if (remainingCalls === 0) {
         throw new Error('Unable to fetch comments at this time. Check back later.')
+    }
 }
 
 module.exports = octokit
