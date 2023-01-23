29b9006a94185ad40b53975556368a441d8a530b
Selfscrum <martin@selfscrum.com>
2023-01-23 01:12:28 +0100

    functions

--- static/scripts/index.js
+++
@@ -1,9 +0,0 @@
-const fetchComments = async (id) => {
-    const response = await fetch(`/.netlify/functions/comments?id=${id}`);
-    const { data: comments, error } = await response.json();
-    if (error) {
-      throw new Error(error);
-    }
-    return comments;
-  };
-  
